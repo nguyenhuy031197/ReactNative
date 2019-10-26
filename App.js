ff35acab9f9f7052daa59a776e0adc0f890605b8
Huy Nguyễn <nguyenhuy031197@gmail.com>
2019-10-26 20:24:17 +0700

    finish bai tap 1

--- App.js
+++ App.js
@@ -1,4 +1,62 @@
 /* eslint-disable react-native/no-inline-styles */
+// import React, {Component} from 'react';
+// import {View, Text} from 'react-native';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <View style={{flex: 1, flexDirection: 'column'}}>
+//         <View
+//           style={{flex: 1, backgroundColor: 'red', flexDirection: 'column'}}>
+//           <Text
+//             style={{
+//               flex: 1,
+//               backgroundColor: 'yellow',
+//               color: 'black',
+//               textAlign: 'center',
+//               textAlignVertical: 'center',
+//             }}>
+//             A
+//           </Text>
+//           <Text
+//             style={{
+//               flex: 1,
+//               backgroundColor: 'green',
+//               color: 'black',
+//               textAlign: 'center',
+//               textAlignVertical: 'center',
+//             }}>
+//             B
+//           </Text>
+//           <Text
+//             style={{
+//               flex: 1,
+//               backgroundColor: 'gray',
+//               color: 'black',
+//               textAlign: 'center',
+//               textAlignVertical: 'center',
+//             }}>
+//             C
+//           </Text>
+//           <Text
+//             style={{
+//               flex: 1,
+//               backgroundColor: 'pink',
+//               color: 'black',
+//               textAlign: 'center',
+//               textAlignVertical: 'center',
+//             }}>
+//             D
+//           </Text>
+//         </View>
+//         <View style={{flex: 1, backgroundColor: 'blue'}} />
+//       </View>
+//     );
+//   }
+// }
+
+// export default App;
+/* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
@@ -49,11 +107,50 @@ class App extends Component {
             D
           </Text>
         </View>
-        <View style={{flex: 1, backgroundColor: 'blue'}} />
+        <View style={{flex: 1, backgroundColor: 'blue', flexDirection: 'row'}}>
+          <Text
+            style={{
+              flex: 1,
+              backgroundColor: 'yellow',
+              color: 'black',
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            A
+          </Text>
+          <Text
+            style={{
+              flex: 1,
+              backgroundColor: 'green',
+              color: 'black',
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            B
+          </Text>
+          <Text
+            style={{
+              flex: 1,
+              backgroundColor: 'gray',
+              color: 'black',
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            C
+          </Text>
+          <Text
+            style={{
+              flex: 1,
+              backgroundColor: 'pink',
+              color: 'black',
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            D
+          </Text>
+        </View>
       </View>
     );
   }
 }
-
 export default App;
-
