7a88f08b127ad5d1643de201fc09c7b9e34f418d
Huy Nguyễn <nguyenhuy031197@gmail.com>
2019-10-27 09:42:04 +0700

    feat<Dimensionapp> : get width , height

---
+++ src/unit/Dimensionapp.js
@@ -0,0 +1,14 @@
+import {Dimensions} from 'react-native';
+class Dimensionapp {
+  static getWidth() {
+    return Dimensions.get('window').width;
+  }
+  static getHeight() {
+    return Dimensions.get('window').height;
+  }
+}
+
+// const Dimensionapp1 = {
+//   width: Dimensions.get('window').width,
+//   height: Dimensions.get('window').height,
+// };
