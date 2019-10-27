3be90b85760bdd42d2befe34fda6e64d3098e71e
Huy Nguyễn <nguyenhuy031197@gmail.com>
2019-10-27 09:31:35 +0700

    feat<Word> : css ui

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,7 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
 import {Text, View, TouchableOpacity, Dimensions} from 'react-native';
-import { isMemberExpression } from '@babel/types';
 
 const width = Dimensions.get('window').width;
 
@@ -12,30 +11,51 @@ export default class Word extends Component {
       <View
         style={{
           flexDirection: 'column',
-          height: width / 5,
+          height: width / 4,
           borderColor: 'white',
           borderRadius: width / 100,
           backgroundColor: 'white',
           margin: width / 70,
           justifyContent: 'center',
         }}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            paddingHorizontal: width / 8,
+          }}>
           <Text style={{fontSize: width / 15, color: '#28a745'}}> {en} </Text>
           <Text style={{fontSize: width / 15, color: 'red'}}>
-          {isMemorized ? '----' : vn}
+            {isMemorized ? '----' : vn}
           </Text>
         </View>
         <View
           style={{
             flexDirection: 'row',
-            justifyContent: 'space-around',
+            justifyContent: 'space-between',
             marginTop: width / 30,
+            paddingHorizontal: width / 15,
           }}>
-          <TouchableOpacity>
-          <Text>{isMemorized ? 'Forgot' : 'isMemorized'}</Text>
+          <TouchableOpacity
+            style={{
+              backgroundColor: isMemorized ? '#28a745' : 'red',
+              padding: width / 65,
+              marginLeft: isMemorized ? width / 20 : 0,
+            }}>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: width / 20,
+              }}>
+              {isMemorized ? 'Forgot' : 'isMemorized'}
+            </Text>
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Text>Remove</Text>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#E0A800',
+              padding: width / 65,
+            }}>
+            <Text style={{color: 'white', fontSize: width / 20}}>Remove</Text>
           </TouchableOpacity>
         </View>
       </View>
