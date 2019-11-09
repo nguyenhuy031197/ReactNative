45fa0208f695a55afdc864cbd673162343ca9e27
Huy Nguyễn <nguyenhuy031197@gmail.com>
2019-11-09 15:14:16 +0700

    myfunction component

--- src/components/MyState.js
+++ src/components/MyState.js
@@ -1,6 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
+import {Text, View} from 'react-native';
+import Myfunction from './Myfunction';
 
 export default class MyState extends Component {
   constructor(props) {
@@ -8,6 +9,15 @@ export default class MyState extends Component {
     this.state = {
       count: 1,
     };
+  }
+  inCrease() {
+    this.setState({count: this.state.count + 1});
+  }
+  deCrease() {
+    this.setState({count: this.state.count - 1});
+  }
+  reset() {
+    this.setState({count: 0});
   }
   render() {
     return (
@@ -21,36 +31,7 @@ export default class MyState extends Component {
         <Text style={{color: 'red', fontSize: 40}}>
           Count : {this.state.count}
         </Text>
-        <View
-          style={{
-            width: '100%',
-            flexDirection: 'row',
-            justifyContent: 'space-evenly',
-          }}>
-          <TouchableOpacity
-            style={{backgroundColor: 'green', padding: 10}}
-            onPress={function() {
-              count++;
-              alert(count);
-            }}>
-            <Text
-              style={{color: 'white', fontStyle: 'italic', fontWeight: 'bold'}}>
-              Increase
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{backgroundColor: 'red', padding: 10}}>
-            <Text
-              style={{color: 'white', fontStyle: 'italic', fontWeight: 'bold'}}>
-              Decrease
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{backgroundColor: 'slategray', padding: 10}}>
-            <Text
-              style={{color: 'white', fontStyle: 'italic', fontWeight: 'bold'}}>
-              Reset
-            </Text>
-          </TouchableOpacity>
-        </View>
+        <Myfunction />
       </View>
     );
   }
